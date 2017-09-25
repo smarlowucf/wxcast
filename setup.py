2648ae8149db9c8fc0ecc2307d9d265dfac6b9de
Sean Marlow <smarlowucf@gmail.com>
2017-09-24 20:48:58 -0600

    Bump version: 1.0.3 → 1.0.4

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ test_requirements = [
 
 setup(
     name='wxcast',
-    version='1.0.3',
+    version='1.0.4',
     description='A CLI utility for retrieving weather information.',
     long_description=readme + '\n\n' + changes,
     author="Sean Marlow",
--- wxcast/__init__.py
+++ wxcast/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
 __author__ = """Sean Marlow"""
-__version__ = '1.0.3'
+__version__ = '1.0.4'
